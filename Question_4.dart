736b6029c276627132ee6fafdc4699ce0402b87b
Bilal0718 <bilalpeermuhammad@gmail.com>
2024-01-01 12:44:55 +0500

    Q4

---
+++ Question_4.dart
@@ -0,0 +1,5 @@
+void main() {
+double temperatureCelsius = 33.6;
+  double temperatureFahrenheit = (temperatureCelsius * 9/5) + 32;
+  print('Temperature in Fahrenheit: $temperatureFahrenheit°F');
+}
