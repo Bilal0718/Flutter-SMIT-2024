343fe952a24170bcd9ff350c77c2b8ef84418849
Bilal0718 <bilalpeermuhammad@gmail.com>
2024-01-08 19:32:21 +0500

    Changes to Quiz1

--- Quiz1.dart
+++ Quiz1.dart
@@ -1,12 +1,14 @@
 void main(){
-  String Email = 'bilalpeermuhammad@gmail.com';
-  String Password = 'karachi125';
+  String Email = 'bilalpeermuhammad1@gmail.com';
+  String Password = 'karachi122';
 
   if(Email == 'bilalpeermuhammad@gmail.com' && Password == 'karachi125'){
     print('All Correct Access granted');
   } else if(Email == 'bilalpeermuhammad@gmail.com' && Password != 'karachi125'){
     print('Password Incorrect');
-  } else{
+  } else if(Email != 'bilalpeermuhammad@gmail.com' && Password == 'karachi125'){
     print('Email Incorrect');
-  } 
+  } else{
+    print('Both Incorrect');
+  }
 }
