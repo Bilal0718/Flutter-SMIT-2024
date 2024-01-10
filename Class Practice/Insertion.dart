d43ac9b2759166a8d4f10a9b2797c7afe8986aa8
Bilal <152738961+Bilal0718@users.noreply.github.com>
2024-01-10 20:27:52 +0500

    Add files via upload

---
+++ Class Practice/Insertion.dart
@@ -0,0 +1,5 @@
+void main(){
+  var StudentNames = [4,6,3,8,1,9];
+  StudentNames.insert(1, 33);
+  print(StudentNames);
+}
---
+++ Class Practice/Remove.dart
@@ -0,0 +1,7 @@
+void main(){
+  var StudentNames = [4,6,3,8,1,9];
+  StudentNames.remove(3); // value 3 will be removed
+  print(StudentNames);
+  StudentNames.removeAt(2); // index 2 will be removed
+  print(StudentNames);
+}
