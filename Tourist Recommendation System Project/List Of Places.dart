a56dcd8ba4e5f5d94c6a7851f5ac7503192a3fea
Bilal <152738961+Bilal0718@users.noreply.github.com>
2024-02-21 19:41:39 +0500

    Add files via upload

---
+++ Tourist Recommendation System Project/List Of Places.dart
@@ -0,0 +1,50 @@
+
+import 'Tourist Destination Class.dart';
+
+List<TouristDestination> destinations = [
+    TouristDestination(
+        name: "Hawaii",
+        activities: ["Beach", "Snorkeling", "Hiking", "Surfing"],
+        averageCost: 2000,
+        climate: "Tropical"),
+    TouristDestination(
+        name: "Switzerland",
+        activities: ["Skiing", "Hiking", "Sightseeing"],
+        averageCost: 3000,
+        climate: "Temperate"),
+    TouristDestination(
+        name: "Dubai",
+        activities: ["Shopping", "Desert Safari", "Sightseeing"],
+        averageCost: 2500,
+        climate: "Desert"),
+    TouristDestination(
+      name: "Rio de Janeiro",
+      activities: ["Christ the Redeemer", "Copacabana Beach", "Sugarloaf Mountain"],
+      averageCost: 2700,
+      climate: "Tropical",
+    ),
+    TouristDestination(
+      name: "Rome",
+      activities: ["Colosseum", "Vatican City", "Trevi Fountain"],
+      averageCost: 3200,
+      climate: "Mediterranean",
+    ),
+    TouristDestination(
+      name: "Tokyo",
+      activities: ["Shibuya Crossing", "Tokyo Disneyland", "Senso-ji Temple"],
+      averageCost: 2800,
+      climate: "Temperate",
+    ),
+    TouristDestination(
+      name: "New York City",
+      activities: ["Times Square", "Statue of Liberty", "Central Park"],
+      averageCost: 3500,
+      climate: "Temperate",
+    ),
+    TouristDestination(
+      name: "Paris",
+      activities: ["Eiffel Tower", "Louvre Museum", "Seine River Cruise"],
+      averageCost: 3000,
+      climate: "Temperate",
+    ),
+  ];
