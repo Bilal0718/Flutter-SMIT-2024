1d552129887b862c7edc2a16355dedd6d2173be4
Bilal <152738961+Bilal0718@users.noreply.github.com>
2024-02-26 19:12:57 +0500

    Add files via upload

---
+++ Tourist Recommendation System Project/main.dart
@@ -0,0 +1,12 @@
+//ignore_for_file: unused_local_variable
+//Aik hi kaam ho sakta he sirf aik run pe ya to signup hoga ya tou login
+//file mein store nahi kiya ha isiliye new user register karke login nahi hosakta
+import 'choice.dart';
+
+void main() {
+  Map<String, String> Users = {'bilal@gmail.com':'bilal123',
+  'hamza761@gmail.com':'hamza541',
+  'zain342@gmail.com':'zain4351'
+  };
+  choice(Users);
+}
