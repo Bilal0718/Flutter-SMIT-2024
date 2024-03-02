7936f6fb48560dc32d8fe3386655539e77c73a2e
Bilal <152738961+Bilal0718@users.noreply.github.com>
2024-03-02 15:52:41 +0500

    Add files via upload

---
+++ Tourist Recommendation System Project/Tourist Destination Class.dart
@@ -0,0 +1,12 @@
+class TouristDestination {
+  final String name;
+  final List<String> activities;
+  final int averageCost;
+  final String climate;
+
+  TouristDestination(
+      {required this.name,
+      required this.activities,
+      required this.averageCost,
+      required this.climate});
+}
